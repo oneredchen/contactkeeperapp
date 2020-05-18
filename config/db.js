fa9bdc54b1ee9c8ed046c80a913e6736032f3455
Chenny <hatrend@hotmail.com>
2020-05-18 19:49:36 +0800

    MongoDB Atlas Connection Set Up Completed

---
+++ config/db.js
@@ -0,0 +1,20 @@
+//For handling connections to the MongoDB Atlas
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI')
+
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(db, {
+             useUnifiedTopology: true,
+             useNewUrlParser: true
+        });
+        
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
